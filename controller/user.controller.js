f0fe784b26bb6d52cd309c4b2527adf882567979
Shakiba Alam <shakibaalam092@gmail.com>
2022-09-02 22:15:08 +0600

    api

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -25,35 +25,55 @@ let users = [
     },
 ]
 
+//Get all the users
 module.exports.getAllUsers = (req, res) => {
     const { limit } = req.query
     res.json(users.slice(0, limit))
 }
 
+//Save a user
 module.exports.postAUser = (req, res) => {
+    const { id, gender, name, contact, address, photoUrl } = req.body
     // console.log(req.body);
-    users.push(req.body)
-    res.send(users)
+    if (id, gender, name, contact, address, photoUrl) {
+        users.push(req.body)
+        res.send(users)
+    }
+    else {
+        res.send('Give all the properties like : id, gender, name, contact, address, photoUrl')
+    }
 }
 
+//Get a random user
 module.exports.getAUser = (req, res) => {
     const { id } = req.params;
     const foundUser = users.find(u => u.id === Number(id))
-    res.send(foundUser)
+    if (foundUser) {
+        res.send(foundUser)
+    }
+    else {
+        res.send("user id doesn't exist")
+    }
 }
 
+//Update a user's information
 module.exports.updateAUser = (req, res) => {
     const { id } = req.params;
     let newData = users.find(u => u.id === Number(id))
-    newData = {
-        id: req.body.id || id,
-        gender: req.body.gender || newData.gender,
-        name: req.body.name || newData.name,
-        contact: req.body.contact || newData.contact,
-        address: req.body.address || newData.address,
-        photoUrl: req.body.photoUrl || newData.photoUrl
+    if (newData) {
+        newData = {
+            id: req.body.id || id,
+            gender: req.body.gender || newData.gender,
+            name: req.body.name || newData.name,
+            contact: req.body.contact || newData.contact,
+            address: req.body.address || newData.address,
+            photoUrl: req.body.photoUrl || newData.photoUrl
+        }
+        res.send(newData)
     }
-    res.send(newData)
+    else {
+        res.send("user id doesn't exist")
+    }
 }
 
 module.exports.deleteAUser = (req, res) => {
